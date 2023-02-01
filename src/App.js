809bd1d4d873f386add94557d95347044b6766fc
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-02-01 20:56:01 +0330

    create range MUI component and use it conditionally in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Caption from './components/Caption';
 import MainWrapper from './components/MainWrapper';
 import Select from "./components/Select/Select"
+import Range from "./components/Range/Range"
+import { useSelector } from 'react-redux';
 
 
 function App() {
+
+  const counter = useSelector((state) => state.questions.index)
+  const data = useSelector((state) => state.questions.datas)
+
+  const questionType = data[counter].type
+
+
+  console.log(questionType)
 
   return (
     <div className="App">
       <MainWrapper>
         <Caption/>
-        <Select/>
+        {questionType === "Drop_Down" && <Select/>}
+        {questionType === "Range" && <Range/>}
       </MainWrapper>
     </div>
   );
